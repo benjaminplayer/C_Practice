878698199a8aade62b8a8ec67ceffc9f207f502c
Benjamin <fireblazegaming21@gmail.com>
2025-08-24 04:06:14 +0200

    commit 1

--- Functions.c
+++ Functions.c
@@ -27,17 +27,26 @@ int gcd(int a, int b)
     return a;
 }
 
+int fib(int n)
+{
+    if(n == 0) return 0;
+    if(n == 1 || n == 2) return 1;
+    else return fib(n-2) + fib(n-1);
+}
+
 int main()
 {
-    int a,b;
-    
+    int a;
+    //int b;
     printf("Enter a number \n");
     scanf("%d",&a);
-    printf("Enter a second number \n");
+    /*printf("Enter a second number \n");
     scanf("%d",&b);
     //(isPrime(a)) ? printf("%d is a prime",a) : ("%d is not a prime",a);
     //printf("%d",factorial(a));
-    printf("gcd: %d",gcd(a,b));
+    //printf("gcd: %d",gcd(a,b));
+    */
+    printf("%d", fib(a));
 
     return 0;
 }
